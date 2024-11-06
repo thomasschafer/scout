0d54aefb42ab412564c94baf3585404dfabe15e4
thomasschafer <thomasschafer97@gmail.com>
2024-11-06 20:14:24 +0000

    Add test for filtering by directory

--- tests/app.rs
+++ tests/app.rs
@@ -2,8 +2,11 @@ use scout::{
     App, CurrentScreen, EventHandler, ReplaceResult, ReplaceState, Results, SearchFields,
     SearchResult, SearchState,
 };
+use std::fs;
+use std::fs::create_dir_all;
 use std::fs::File;
 use std::io::Write;
+use std::path::Path;
 use std::path::PathBuf;
 use tempfile::TempDir;
 
@@ -93,25 +96,27 @@ async fn test_app_reset() {
     assert!(matches!(app.results, Results::Loading));
 }
 
-macro_rules! define_files {
-    ($($name:expr => {$($line:expr),+ $(,)?}),+ $(,)?) => {
-        [
+macro_rules! create_test_files {
+    ($temp_dir:expr, $($name:expr => {$($line:expr),+ $(,)?}),+ $(,)?) => {
+        {
             $(
-                (
-                    $name,
-                    concat!(
-                        $($line,"\n",)+
-                    )
-                ),
+                let contents = concat!($($line,"\n",)+);
+                let path = [$temp_dir.path().to_str().unwrap(), $name].join("/");
+                let path = Path::new(&path);
+                create_dir_all(path.parent().unwrap()).unwrap();
+                let mut file = File::create(path).unwrap();
+                file.write_all(contents.as_bytes()).unwrap();
+                file.sync_all().unwrap();
             )+
-        ]
+        }
     };
 }
 
-fn setup_test_environment() -> App {
+fn setup_env_simple_files() -> App {
     let temp_dir = TempDir::new().unwrap();
 
-    for (name, contents) in define_files! {
+    create_test_files! {
+        temp_dir,
         "file1.txt" => {
             "This is a test file",
             "It contains some test content",
@@ -127,12 +132,7 @@ fn setup_test_environment() -> App {
             "123 bar[a-b]+.*bar)(baz 456",
             "something",
         }
-    } {
-        let path = temp_dir.path().join(name);
-        let mut file = File::create(path).unwrap();
-        file.write_all(contents.as_bytes()).unwrap();
-        file.sync_all().unwrap();
-    }
+    };
 
     let events = EventHandler::new();
     App::new(Some(temp_dir.into_path()), events.app_event_sender)
@@ -140,7 +140,7 @@ fn setup_test_environment() -> App {
 
 #[tokio::test]
 async fn test_update_search_results_fixed_string() {
-    let mut app = setup_test_environment();
+    let mut app = setup_env_simple_files();
 
     app.search_fields = SearchFields::with_values(".*", "example", true, "");
 
@@ -171,7 +171,7 @@ async fn test_update_search_results_fixed_string() {
 
 #[tokio::test]
 async fn test_update_search_results_regex() {
-    let mut app = setup_test_environment();
+    let mut app = setup_env_simple_files();
 
     app.search_fields = SearchFields::with_values(r"\b\w+ing\b", "VERB", false, "");
 
@@ -214,7 +214,7 @@ async fn test_update_search_results_regex() {
 }
 #[tokio::test]
 async fn test_update_search_results_no_matches() {
-    let mut app = setup_test_environment();
+    let mut app = setup_env_simple_files();
 
     app.search_fields = SearchFields::with_values("nonexistent", "replacement", false, "");
 
@@ -229,7 +229,7 @@ async fn test_update_search_results_no_matches() {
 
 #[tokio::test]
 async fn test_update_search_results_invalid_regex() {
-    let mut app = setup_test_environment();
+    let mut app = setup_env_simple_files();
 
     app.search_fields = SearchFields::with_values(r"[invalid regex", "replacement", false, "");
 
@@ -237,6 +237,74 @@ async fn test_update_search_results_invalid_regex() {
     assert!(result.is_ok());
 }
 
+fn setup_env_files_in_dirs() -> App {
+    let temp_dir = TempDir::new().unwrap();
+
+    create_test_files! {
+        temp_dir,
+        "dir1/file1.txt" => {
+            "This is a test file",
+            "It contains some test content",
+            "For testing purposes",
+        },
+        "dir2/file2.txt" => {
+            "Another test file",
+            "With different content",
+            "Also for testing",
+        },
+        "dir2/file3.txt" => {
+            "something",
+            "123 bar[a-b]+.*bar)(baz 456",
+            "something testing",
+        }
+    };
+
+    for dir in fs::read_dir(temp_dir.path()).unwrap() {
+        for path in fs::read_dir(dir.unwrap().path()).unwrap() {
+            println!("Name: {}", path.unwrap().path().display())
+        }
+    }
+
+    let events = EventHandler::new();
+    App::new(Some(temp_dir.into_path()), events.app_event_sender)
+}
+
+#[tokio::test]
+async fn test_update_search_results_filtered_dir() {
+    let mut app = setup_env_files_in_dirs();
+
+    app.search_fields = SearchFields::with_values(r"testing", "f", false, "dir2");
+
+    let result = app.update_search_results();
+    assert!(result.is_ok());
+
+    if let scout::Results::SearchComplete(search_state) = &app.results {
+        assert_eq!(search_state.results.len(), 2);
+
+        for (file_name, num_matches) in [
+            ("dir1/file1.txt", 0),
+            ("dir2/file2.txt", 1),
+            ("dir2/file3.txt", 1),
+        ] {
+            println!("Results: {:?}", search_state.results);
+            assert_eq!(
+                search_state
+                    .results
+                    .iter()
+                    .filter(|r| r.path.to_str().unwrap().contains(file_name))
+                    .count(),
+                num_matches
+            );
+        }
+
+        for result in &search_state.results {
+            assert_eq!(result.replacement, result.line.replace("testing", "f"));
+        }
+    } else {
+        panic!("Expected SearchComplete results");
+    }
+}
+
 // TODO: add tests for:
 // - replacing in files
-// - passing in directory via CLI arg
+// - more tests for passing in directory via CLI arg
