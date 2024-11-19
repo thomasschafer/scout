9c07e36b6ba67b7815df9a324da662125d2385a0
thomasschafer <thomasschafer97@gmail.com>
2024-11-19 12:41:51 +0000

    Clean up events

--- src/event.rs
+++ src/event.rs
@@ -7,26 +7,24 @@ use tokio::sync::mpsc;
 #[derive(Clone, Debug)]
 pub enum Event {
     Key(KeyEvent),
-    Mouse(MouseEvent),
-    Resize(u16, u16),
     App(AppEvent),
+    #[allow(dead_code)]
+    Mouse(MouseEvent),
+    #[allow(dead_code)]
+    Resize(u16, u16),
 }
 
 #[derive(Debug)]
 pub struct EventHandler {
-    pub sender: mpsc::UnboundedSender<Event>,
     receiver: mpsc::UnboundedReceiver<Event>,
     pub app_event_sender: mpsc::UnboundedSender<AppEvent>,
-    #[allow(dead_code)]
-    handler: tokio::task::JoinHandle<()>,
 }
 
 impl EventHandler {
     pub fn new() -> Self {
         let (sender, receiver) = mpsc::unbounded_channel();
         let (app_event_sender, mut app_event_receiver) = mpsc::unbounded_channel();
-        let _sender = sender.clone();
-        let handler = tokio::spawn(async move {
+        tokio::spawn(async move {
             let mut reader = event::EventStream::new();
             loop {
                 tokio::select! {
@@ -34,30 +32,28 @@ impl EventHandler {
                         match evt {
                             CrosstermEvent::Key(key) => {
                                 if key.kind == crossterm::event::KeyEventKind::Press {
-                                    _sender.send(Event::Key(key)).unwrap();
+                                    sender.send(Event::Key(key)).unwrap();
                                 }
                             },
                             CrosstermEvent::Mouse(mouse) => {
-                                _sender.send(Event::Mouse(mouse)).unwrap();
+                                sender.send(Event::Mouse(mouse)).unwrap();
                             },
                             CrosstermEvent::Resize(x, y) => {
-                                _sender.send(Event::Resize(x, y)).unwrap();
+                                sender.send(Event::Resize(x, y)).unwrap();
                             },
                             _ => {}
                         }
                     }
                     Some(app_evt) = app_event_receiver.recv() => {
-                        _sender.send(Event::App(app_evt)).unwrap();
+                        sender.send(Event::App(app_evt)).unwrap();
                     }
                     else => break,
                 };
             }
         });
         Self {
-            sender,
             receiver,
             app_event_sender,
-            handler,
         }
     }
 
