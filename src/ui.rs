c7b94ce3b0b8479e822a8dcf6b59386dade9b794
thomasschafer <thomasschafer97@gmail.com>
2024-12-04 19:17:17 +0000

    Remove unneeded comment

--- src/ui.rs
+++ src/ui.rs
@@ -449,7 +449,6 @@ pub fn render(app: &App, frame: &mut Frame<'_>) {
         }
         Screen::SearchProgressing(_) | Screen::SearchComplete(_) => {
             let mut keys = if let Screen::SearchComplete(_) = app.current_screen {
-                // TODO: actually prevent confirmation when search is in progress
                 vec!["<enter> replace"]
             } else {
                 vec![]
